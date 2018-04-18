f0ab6a73542cdb3df0783254b122eccae41e3279
Rafael Miranda <rafael.effects@gmail.com>
2018-04-18 21:11:52 +0100

    Small performance improvement.

--- crawler.js
+++ crawler.js
@@ -4,21 +4,24 @@ const fs = require('fs');
 
 moment.locale('pt-br');
 
-const articleListClass = "list-default rect-darkred";
 const articleClass = "list-item";
 
-const totalDaysToFetch = 30;
+const totalDaysToFetch = 2;
 let articlesFound =[];
 let pageNumber = 1;
 let lastArticleDate = moment();
 
 (async () => {
+    console.log("Initializing...");
+    let startTime = moment();
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
     while (lastArticleDate > moment().subtract(totalDaysToFetch, 'days')) {
+        console.log("Fetching new page...");
         await page.goto('https://www.empiricus.com.br/conteudo/newsletters/page/' + pageNumber, {"waitUntil" : "networkidle0"});
 
+        console.log("Fetching articles...");
         let articlesOnPage = await page.evaluate((sel) => {
             return document.getElementsByClassName(sel).length;
         }, articleClass);
@@ -33,74 +36,79 @@ let lastArticleDate = moment();
             await page.evaluate('window.scrollBy(0, '+ articleItemHeight +')');
             await page.waitFor(2000);
 
-            //Imagem
-            let image = "data:";
-            while (image.includes('data:')) {
-                image = await page.evaluate((selector) => {
-                    let e = document.querySelector(selector).getAttribute('src');
-                    return e ? e : null;
-                }, "ul.list-default > li:nth-child(" + i + ") div.list-item--thumb > img");
-                await page.waitFor(500);
-            }
-
-            //Titulo
-            let title = await page.evaluate((selector) => {
-                let e = document.querySelector(selector);
-                return e ? e.innerHTML : null;
-            }, "ul.list-default > li:nth-child(" + i + ") div.list-item--meta > h2");
-
             //Data
             let data = await page.evaluate((selector) => {
                 let e = document.querySelector(selector);
                 return e ? e.innerHTML.split(' - ')[1] : null;
             }, "ul.list-default > li:nth-child(" + i + ") div.list-item--meta > p.list-item--info");
 
-            //Descrição
-            let description = await page.evaluate((selector) => {
-                let e = document.querySelector(selector);
-                return e ? e.innerHTML : null;
-            }, "ul.list-default > li:nth-child(" + i + ") div.list-item--meta > p.list-item--description");
-
-            //URL
-            let url = await page.evaluate((selector) => {
-                let e = document.querySelector(selector).getAttribute('href');
-                return e ? e : null;
-            }, "ul.list-default > li:nth-child(" + i + ") article.list-item > a");
-
-            //HTML da notícia
-            let articlePage = await browser.newPage();
-            await articlePage.goto(url, {"waitUntil" : "networkidle0"});
-            let html = await articlePage.evaluate((selector) => {
-                let e = document.querySelector(selector);
-                return e ? e.innerHTML : null;
-            }, "article.article > div#article-content");
-            await articlePage.close();
-
             if (moment(data, "DD ** MMMM, YYYY") > moment().subtract(totalDaysToFetch, 'days')) {
-                await articlesFound.push({
+
+                //Titulo
+                let title = await page.evaluate((selector) => {
+                    let e = document.querySelector(selector);
+                    return e ? e.innerHTML : null;
+                }, "ul.list-default > li:nth-child(" + i + ") div.list-item--meta > h2");
+
+                //Descrição
+                let description = await page.evaluate((selector) => {
+                    let e = document.querySelector(selector);
+                    return e ? e.innerHTML : null;
+                }, "ul.list-default > li:nth-child(" + i + ") div.list-item--meta > p.list-item--description");
+
+                //URL
+                let url = await page.evaluate((selector) => {
+                    let e = document.querySelector(selector).getAttribute('href');
+                    return e ? e : null;
+                }, "ul.list-default > li:nth-child(" + i + ") article.list-item > a");
+
+                //HTML da notícia
+                let articlePage = await browser.newPage();
+                await articlePage.goto(url, {"waitUntil" : "networkidle0"});
+                let html = await articlePage.evaluate((selector) => {
+                    let e = document.querySelector(selector);
+                    return e ? e.innerHTML : null;
+                }, "article.article > div#article-content");
+                await articlePage.close();
+
+                //Imagem
+                let image = "data:";
+                while (image.includes('data:')) {
+                    image = await page.evaluate((selector) => {
+                        let e = document.querySelector(selector).getAttribute('src');
+                        return e ? e : null;
+                    }, "ul.list-default > li:nth-child(" + i + ") div.list-item--thumb > img");
+                    await page.waitFor(500);
+                }
+
+                articlesFound.push({
                     imagem: image,
                     titulo: title,
                     data: data,
                     descricao: description,
                     url: url,
-                    html: html
+                    html: null
                 });
 
-                // console.log(articlesFound);
+                console.log("Article fetched.");
+                console.log("Article date: " + data);
             }
 
             lastArticleDate = moment(data, "DD ** MMMM, YYYY");
+            if (lastArticleDate < moment().subtract(totalDaysToFetch, 'days'))
+                i = articlesOnPage;
         }
 
         pageNumber++;
     }
 
-    console.log("Pages crawled: " + pageNumber);
+    console.log("Pages crawled: " + (pageNumber-1));
     console.log("Articles fetched: " + articlesFound.length);
 
     // console.log(articlesFound);
     fs.writeFileSync('./articles.json', JSON.stringify(articlesFound));
     console.log("Articles saved on file 'articles.json'");
+    console.log("Total run time: " + moment.duration(moment().diff(startTime)).asSeconds() + " seconds.");
 
     await browser.close();
 
